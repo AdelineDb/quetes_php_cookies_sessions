bdebad33efacc8b8b6f8cfe97f894566c69245ef
AdelineDb <adeline.dubosc@hotmail.fr>
2019-04-21 21:55:09 +0200

    solution quete

--- cart.php
+++ cart.php
@@ -1,7 +1,23 @@
-<?php require 'inc/head.php'; ?>
+<?php
+
+require 'inc/head.php';  ?>
+
 <section class="cookies container-fluid">
     <div class="row">
-      TODO : Display shopping cart items from $_SESSION here.
+
+      <?php if (!empty($_SESSION['cart'][46])) {
+          echo 'Number of Pecan Nuts cookies : ' . $_SESSION['cart'][46] . '<br>';
+          }
+          if (isset($_SESSION['cart'][49])) {
+          echo 'Number of Chocolate chips cookies : ' . $_SESSION['cart'][49] . '<br>';
+          }
+          if (isset($_SESSION['cart'][58])) {
+          echo 'Number of Chocolate cookies : ' . $_SESSION['cart'][58] . '<br>';
+          }
+          if (isset($_SESSION['cart'][32])) {
+          echo 'Number of M&Ms cookies : ' . $_SESSION['cart'][32] . '<br>';
+
+      } ?>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
